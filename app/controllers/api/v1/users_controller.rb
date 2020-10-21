d7977549ac54b5fad3fb03cacbecc0dba419c9bf
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-21 00:18:10 -0400

    added to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,14 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
+
+    render json: user
+  end
+
+  def show
+    user = User.find(params[:id])
+
+    render json: user
   end
 
   private 
